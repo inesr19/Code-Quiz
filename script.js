0879c735bb3ea62d9cdf072b8139a652756f461a
Ines Radic <inesradic12314@gmail.com>
2020-12-03 15:06:50 -0800

    add quiz functions

--- script.js
+++ script.js
@@ -21,3 +21,56 @@ var questionsEl = [
 		answer: 2
 	}
 ];
+
+// Function
+
+function startQuiz() {
+    console.log("started");
+    startButton.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    showQuestion(currentQuestion);
+   }
+
+document.getElementById("start-button").addEventListener("click", function(){
+var timeLeft = 76;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=timeLeft;
+  timeLeft--;
+  if (timeLeft === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    document.getElementById('count').innerHTML= 'Times up!';
+  }
+}, 1000);
+nextQuestion();
+});
+
+const nextQuestion = () => {
+correct.innerHTML = '';
+wrong.innerHTML = '';
+}
+
+function showQuestion(index) {
+    questionTitle.textContent = questionsEl[index].question;
+    for (var i=0; i < questionsEl[0].choices.length; i++){
+        var answer = document.getElementById("answer" + (i + 1))
+        answer.textContent = questionsEl[0].choices[i];
+    }
+} 
+
+function checkQuestion() {
+    if(this.value !== questionsEl[index].answer) {
+        timeLeft -= 10;
+        wrong.textContent = "Incorrect!"
+        console.log("correct")
+    } else if(this.value === questionsEl[index].answer){
+        timeLeft += 10;
+        correct.textContent = "Correct!"
+    }
+    currentQuestion ++
+
+    setTimeout(function(){
+        nextQuestion();
+    }, 1000)
+}
